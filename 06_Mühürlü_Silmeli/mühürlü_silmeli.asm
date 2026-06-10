79d2cfb5771fd9e7b939e695ad75a146f8ed2a76
musabiyikli <80281756+musabiyikli@users.noreply.github.com>
2026-06-10 16:16:52 +0300

    Add files via upload

---
+++ 06_Mühürlü_Silmeli/mühürlü_silmeli.asm
@@ -0,0 +1,63 @@
+
+; PICBASIC PRO(TM) Compiler 2.60, (c) 1998, 2009 microEngineering Labs, Inc. All Rights Reserved. 
+_USED			EQU	1
+
+	INCLUDE	"C:\PBP\16F628.INC"
+
+RAM_START       		EQU	00020h
+RAM_END         		EQU	0014Fh
+RAM_BANKS       		EQU	00003h
+BANK0_START     		EQU	00020h
+BANK0_END       		EQU	0007Fh
+BANK1_START     		EQU	000A0h
+BANK1_END       		EQU	000EFh
+BANK2_START     		EQU	00120h
+BANK2_END       		EQU	0014Fh
+EEPROM_START    		EQU	02100h
+EEPROM_END      		EQU	0217Fh
+
+R0              		EQU	RAM_START + 000h
+R1              		EQU	RAM_START + 002h
+R2              		EQU	RAM_START + 004h
+R3              		EQU	RAM_START + 006h
+R4              		EQU	RAM_START + 008h
+R5              		EQU	RAM_START + 00Ah
+R6              		EQU	RAM_START + 00Ch
+R7              		EQU	RAM_START + 00Eh
+R8              		EQU	RAM_START + 010h
+FLAGS           		EQU	RAM_START + 012h
+GOP             		EQU	RAM_START + 013h
+RM1             		EQU	RAM_START + 014h
+RM2             		EQU	RAM_START + 015h
+RR1             		EQU	RAM_START + 016h
+RR2             		EQU	RAM_START + 017h
+_PORTL           		EQU	 PORTB
+_PORTH           		EQU	 PORTA
+_TRISL           		EQU	 TRISB
+_TRISH           		EQU	 TRISA
+#define _TRISB??0        	 TRISB, 000h
+#define _TRISA??0        	 TRISA, 000h
+#define _TRISA??1        	 TRISA, 001h
+#define _PORTB??0        	 PORTB, 000h
+#define _PORTA??0        	 PORTA, 000h
+#define _PORTA??1        	 PORTA, 001h
+	INCLUDE	"MHRL_S~1.MAC"
+	INCLUDE	"C:\PBP\PBPPIC14.LIB"
+
+	MOVE?CT	000h, _TRISB??0
+	MOVE?CT	001h, _TRISA??0
+	MOVE?CT	001h, _TRISA??1
+	MOVE?CB	007h, CMCON
+	MOVE?CT	000h, _PORTB??0
+
+	LABEL?L	_X	
+	CMPNE?TCL	_PORTA??0, 000h, L00001
+	MOVE?CT	001h, _PORTB??0
+	LABEL?L	L00001	
+	CMPNE?TCL	_PORTA??1, 000h, L00003
+	MOVE?CT	000h, _PORTB??0
+	LABEL?L	L00003	
+	GOTO?L	_X
+	END?	
+
+	END
